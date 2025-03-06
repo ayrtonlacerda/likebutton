e05fba69ea419a4b2bfb6f6ed19a37e4a3d2ad61
ayrtonlacerda <ayrtonrodrigues.lacerda@gmail.com>
2025-03-06 20:38:36 -0300

    Initial commit

---
+++ src/LikeButton.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+/*const LikeButton = () => {
+  const [likes, setLikes] = React?.useState(0);
+
+  return (
+    <button onClick={() => setLikes(likes + 1)}>React Likes: {likes}</button>
+  );
+};*/
+
+const LikeButton = () => <p>Like Button from Microservice</p>;
+
+export default LikeButton;
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import LikeButton from "./LikeButton.jsx";
+
+export { LikeButton };
+export default LikeButton;
